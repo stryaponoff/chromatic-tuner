16d5734d0cd2543c1019d5cb8d423f3d8b5be576
dtvn sncrk <norzes@mail.ru>
2019-04-24 21:50:58 +0400

    Increase accuracy. Result is bullshit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include "main.h"
 
-const int ocrSampleRate =  F_CPU / (1 * SAMPLE_RATE) - 1;
-volatile int currentData = 0;
-volatile int samples[SAMPLE_COUNT];
+arduinoFFT FFT = arduinoFFT();
+
+volatile const int ocrSampleRate =  F_CPU/SAMPLE_RATE - 1;
+
+double vReal[SAMPLE_COUNT];
+double vImag[SAMPLE_COUNT];
+
 volatile int count = 0;
 volatile bool readyFlag = false;
+volatile int currentData = 0;
+volatile const int magicNumber = 4964;
 
 ISR(TIMER1_COMPA_vect){
     cli();
-    samples[count] = currentData;
+    //PORTB ^= (1<<PB0);
+    vReal[count] = currentData;
+    vImag[count] = 0.0;
     if(count >= SAMPLE_COUNT) {
         readyFlag = true;
         count = 0;
@@ -22,16 +30,15 @@ ISR(TIMER1_COMPA_vect){
 ISR(ADC_vect) {
     cli();
     currentData = ADCH;
-    ADCSRA |= (1 << ADSC); //start ADC measurements
+    ADCSRA |= (1 << ADSC);
     sei();
 }
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Ready");
-
     cli(); 
+    //DDRB |= (1<<PB0);
     timer_setup();
     adc_setup();
     sei();
@@ -41,15 +48,21 @@ void loop()
 {
     if(readyFlag) {
         cli();
+        FFT.Windowing(vReal, SAMPLE_COUNT, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+        FFT.Compute(vReal, vImag, SAMPLE_COUNT, FFT_FORWARD);
+        FFT.ComplexToMagnitude(vReal, vImag, SAMPLE_COUNT);
+        double x = FFT.MajorPeak(vReal, SAMPLE_COUNT, magicNumber);
+        Serial.println(x, 6);
+        delay(500);
         readyFlag = false;
         sei();
     }
 }
 
 void timer_setup() {
-    TCCR1A = 0; 
-    TCCR1B = 0; 
-    TCNT1  = 0; 
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1  = 0;
     OCR1A = ocrSampleRate;
     TCCR1B |= (1 << WGM12);
     TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10);
@@ -67,3 +80,30 @@ void adc_setup() {
     ADCSRA |= (1 << ADEN);
     ADCSRA |= (1 << ADSC);
 }
+
+void print_vector(double *vData, uint16_t bufferSize, uint8_t scaleType)
+{
+    for (uint16_t i = 0; i < bufferSize; i++)
+    {
+        double abscissa;
+        switch (scaleType)
+        {
+            case SCL_INDEX:
+                abscissa = (i * 1.0);
+                break;
+            case SCL_TIME:
+                abscissa = ((i * 1.0) / SAMPLE_RATE);
+                break;
+            case SCL_FREQUENCY:
+                abscissa = ((i * 1.0 * SAMPLE_RATE) / SAMPLE_COUNT);
+                break;
+        }
+        Serial.print(abscissa, 6);
+        if(scaleType==SCL_FREQUENCY)
+            Serial.print("Hz");
+        Serial.print(" ");
+        Serial.println(vData[i], 4);
+    }
+    Serial.println();
+}
+
--- src/main.h
+++ src/main.h
@@ -1,10 +1,17 @@
 #include <Arduino.h>
+#include "arduinoFFT.h"
 
 #define F_CPU 16000000L
 #define SAMPLE_RATE 5000
-#define SAMPLE_COUNT 500
+#define SAMPLE_COUNT 128
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
 
 void setup();
 void loop();
 void timer_setup();
 void adc_setup();
+void print_vector(double *vData, uint16_t bufferSize, uint8_t scaleType);
